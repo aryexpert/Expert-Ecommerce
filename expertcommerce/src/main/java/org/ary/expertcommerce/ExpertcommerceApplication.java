0fd7bae02b9d7de01bb68ce2c06994bd5c09ae98
AryExpert <cedouariel@gmail.com>
2022-05-22 01:55:55 +0200

    Initial commit. created spring boot project and configure the application properties.

---
+++ expertcommerce/src/main/java/org/ary/expertcommerce/ExpertcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.ary.expertcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertcommerceApplication.class, args);
+	}
+
+}
---
+++ expertcommerce/src/test/java/org/ary/expertcommerce/ExpertcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ary.expertcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
